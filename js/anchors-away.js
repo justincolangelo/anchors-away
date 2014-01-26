1025040d6ee0319e7513a39c2178284cf121ad27
justincolangelo <justincolangelo@gmail.com>
2014-01-26 15:34:23 -0500

    Added jquery function definition using $.fn instead of just $.extend.
    Method can be called by $(someId).anchorTo(), $(someId).anchorTo(timeInMS), $(someId).anchorTo(timeInMS, callback)

--- js/anchors-away.js
+++ js/anchors-away.js
@@ -1,12 +1,39 @@
+// Add our new method called as $('#someId').anchorTo(param2[optional], param3[optional])
+jQuery.fn.anchorTo = function(rate, callback){
+     
+    // valid element?
+    var $anchor = $(this.selector).length > 0 ? this : $('html, body');
+    
+    // Valid speed? Default to 1 second
+    var speed = typeof rate === 'number' && rate % 1 == 0 ? rate : 1000;
+    
+    // Valid callback function supplied?
+    var cb = typeof callback == 'function' ? callback : function(){};
+    
+    // Stop any previous scrolling, animate to anchor
+    $('html, body').stop()
+        .animate(
+            { 
+                'scrollTop': $anchor.offset().top 
+            }, 
+            speed, 
+            'swing', // TODO: write options as parameter
+            cb
+        );
+        
+};
+
+
+// Second way to do this 
 jQuery.extend(
     {
 
         // Add our new method called as $.anchorTo(param1, param2[optional], param3[optional])
         anchorTo : function(toId, rate, callback){
-
+            console.log($(toId).length);
             // setup id to navigate
-            var $anchor = $('#' + toId);
-            
+            var $anchor = toId.length && toId.indexOf('#') == 0 ? $(toId) : $('#' + toId).length > 0 ? $('#' + toId) : $('html, body');
+                
             // Valid speed? Default to 1 second
             var speed = typeof rate === 'number' && rate % 1 == 0 ? rate : 1000;
             
